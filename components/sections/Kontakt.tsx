0ccef4fa32ca186a90853d15ea6d0a9c9a71bb0d
LukyGryc <lukygryc@seznam.cz>
2026-01-23 00:16:14 +0100

    sluzby first draft

---
+++ components/sections/Kontakt.tsx
@@ -0,0 +1,9 @@
+const Kontakt = () => {
+  return (
+    <section id="kontakt" className="bg-[url(/wooden-texture.jpg)]  bg-cover min-h-screen ">
+
+    </section>
+  )
+}
+
+export default Kontakt
--- components/sections/ONas.tsx
+++ components/sections/ONas.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 const ONas = () => {
   return (
-    <section className="bg-[url(/wooden-texture.jpg)] bg-cover h-screen">
-      <div className="w-1/2 h-auto p-20 bg-black/70 relative top-50">
+    <section id="onas" className="bg-[url(/woodworker.jpg)] bg-cover min-h-screen ">
+      <div className="h-auto p-10 md:p-20 bg-black/70 relative top-20 md:top-40 sm:w-full md:w-3/4 lg:w-1/2">
         <h1 className="section-header">Vítáme Vás na naších stránkách.</h1>
         <p className="section-paragraph">Jsme součástí zavedené firny, která se od roku 2000 zabývá rekonstrukcí bytových jader, rekonstrukcí bytů a domů. Od roku 2018 jsme rozšířili naši nabídku o truhlářské služby.</p>
         <p className="section-paragraph">Hlavní specializací truhlářské dílny je zakázková výroba kuchyní, koupelnového nábytku, vestavěných skříní, vybavení kanceláří a jiného nábytku z lamina. Dále poskytujeme služby nejen domácím kutilům a to možnost nechat si u nás zadat výrobu nařezání a olepení dílců z lamino desek.</p>
