adb3c6ed9185a8ea4bdf37c08a7b3c665f78e860
LukyGryc <36476593+LukyGryc@users.noreply.github.com>
2026-01-24 00:13:09 +0100

    Update components/layout/Navbar.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,7 +11,7 @@ const links: NavLinkType[] = [
 const Navbar = () => {
   return (
     <header className="fixed top-0 z-50 flex justify-between w-full px-4 md:px-12 lg:px-60 py-3 md:py-4 bg-white/30 text-black backdrop-blur-md items-center">
-      <h1 className="scroll-m-20 text-l md:text-2xl font-semibold">
+      <h1 className="scroll-m-20 text-lg md:text-2xl font-semibold">
         <a href="#landingPage" className="focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 rounded">
           <span>Truhlářství Gryč</span>
         </a>
